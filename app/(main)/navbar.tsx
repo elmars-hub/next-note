d6ffab096b500ac1a2352e8d7f787260e50c392e
Martin Elmars <121293732+elmars-hub@users.noreply.github.com>
2025-10-20 14:21:55 +0100

    fix: navbar and public image

--- app/(main)/navbar.tsx
+++ app/(main)/navbar.tsx
@@ -1,4 +1,4 @@
-import logo from "@/assets/logo.png";
+import logo from "@/public/logo.png";
 import { ModeToggle } from "@/components/mode-toggle";
 import Image from "next/image";
 import Link from "next/link";
